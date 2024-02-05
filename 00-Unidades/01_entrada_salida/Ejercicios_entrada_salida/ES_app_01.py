e1217bc707af53a69df56ba7b7c869bf33dc71c2
vxlentino <valentinocarrazana@gmail.com>
2024-02-05 15:37:55 -0300

    E/S 01 Actualizado

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -22,7 +22,7 @@ class App(customtkinter.CTk):
         self.title("UTN FRA")
 
         self.btn_mostrar = customtkinter.CTkButton(
-            master=self, text="Mostrar", command=self.btn_mostrar_on_click)
+            master=self, text="Hola UTN", command=self.btn_mostrar_on_click)
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
